22b08d49ec6dd9a0dca6c364bb9f67f28f7f4983
312-21\dbrwls12 <dbrwls12@gmail.com>
2024-12-26 10:06:58 +0900

    파이썬 패키지 연습(클래스, 모듈, __init__)

---
+++ game/__init__.py
@@ -0,0 +1,14 @@
+from .graphic .render import render_test
+
+VERSION = 3.11
+
+def print_version_info() :
+    print(f"The version of this game is {VERSION}")
+    print(" 게임 제작자 : 진")
+    print(" 게임 제작자 이메일 : dwid@nsnanw.com")
+    print(" 게임 제작의뢰  : 010-1234-1234")
+
+# 패키지 실행 초기화 문구
+print("Initializing game...")
+print("Loading.............")
+
